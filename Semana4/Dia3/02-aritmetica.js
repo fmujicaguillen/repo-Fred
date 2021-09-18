ad2b174b37c8933016ab5cbb69d659b0a679d80e
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-18 18:59:01 -0500

    updat

---
+++ Semana4/Dia3/02-aritmetica.js
@@ -0,0 +1,19 @@
+let a = 100;
+let b = 4;
+
+let suma = a + b;
+console.log("esto es una suma, ",suma);
+
+let resta = a - b;
+console.log("esto es una resta,", resta)
+
+let multiplicacion = a * b;
+console.log("esto es una multiplicacion, ",multiplicacion);
+
+
+let division = a / b;
+console.log("esto es una division, ", division);
+
+
+let residuo = 105 % b;
+console.log("Este es el residuo, ", residuo)
