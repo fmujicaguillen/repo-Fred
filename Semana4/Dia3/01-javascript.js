530c8da1e70444388b0f7fcfcc75ee4e5ead8fb5
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-18 18:02:49 -0500

    update

---
+++ Semana4/Dia3/01-javascript.js
@@ -0,0 +1,22 @@
+console.log("Hola mundo");
+//variables
+var nombre = "Fred";
+
+console.log(nombre);
+
+var nombre2 = "Juan";
+var edad = 34;
+
+console.log(edad);
+
+var esDeNoche = true;
+console.log(esDeNoche);
+
+
+// para ver el tipo de valor
+console.log(typeof esDeNoche);
+
+//cambiar el tipo de valor de la variable
+
+edad  = 25
+console.log(edad)
