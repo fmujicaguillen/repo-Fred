4b139b8cd13bbe66f4a0dbeb3b5a3453ea9b67bb
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-24 18:50:11 -0500

    update

---
+++ Semana4/Semana5/Dia2/02-ternario.js
@@ -0,0 +1,12 @@
+let edad = 19;
+
+// if(edad >= 18){
+//     console.log("dni AZUL");
+// }else{
+//     console.log("dni amarillo");
+// }
+
+//OPERADOR TERNARIO
+// condicional ? si es verdadero : si es falso
+
+edad >= 18 ? console.log("dni azul") : console.log("dni amarillo")
