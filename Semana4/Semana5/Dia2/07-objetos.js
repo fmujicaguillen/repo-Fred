0dec6ff16a1d8535a20ead3c80f4b98ccc5f2e2b
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-24 21:56:39 -0500

    update

---
+++ Semana4/Semana5/Dia2/07-objetos.js
@@ -0,0 +1,27 @@
+let mascota = {
+    nombre:"Backus",
+    raza:"Poodle",
+    color:"Blanco",
+    edad:13,
+    hobbies:["jugar","Perseguir gatos","Comer pate"],
+    comer: function(){
+        console.log("ñam ñam ñam");
+    },
+    ladra: true,   
+};
+
+//objeto.propiedad
+// console.log(mascota.nombre);
+// console.log(mascota.edad);
+// console.log(mascota.hobbies[1]);
+// mascota.comer(); // para ejecutar una funcion se usan parentesis
+
+let albumMusical = {
+    nombre:"After Hours",
+    fechaLanzamiento:"20/03/2020",
+    genero:["Dream Pop","Drum and Bass"],
+    
+
+
+
+}
