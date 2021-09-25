6498d77291a3bfafea28ae2706d176bc2a09275f
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-24 19:07:40 -0500

    update

---
+++ Semana4/Semana5/Dia2/03-while.js
@@ -0,0 +1,16 @@
+//WHILE
+
+// let contador = 1;
+// //while(condicional)
+// while(contador <= 15){
+//     //si es verdadero
+//     console.log(`El contador es :${contador}`);
+//     //NECESARIAMENTE, tengo q haer un cambio para que la condicional sea falsa en algun momento
+//     contador = contador + 2;
+// }
+
+let count = 1;
+do{
+    console.log(`El contador es: ${count}`)
+    count = count + 1
+} while(count <= 5)
