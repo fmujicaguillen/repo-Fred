87b839c78aae1a750a8c4c4a954ab2ead61b8861
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-24 19:31:26 -0500

    update

---
+++ Semana4/Semana5/Dia2/04-for.js
@@ -0,0 +1,18 @@
+// // for(contador; condicional ; actualizacion_contador){
+//     codigo que se va a repetir
+// }
+//i++    ===== i = i + 1
+
+for (let i = 0 ; i <= 1; i++){
+    console.log(`El contador es : ${i}`);
+
+}
+console.log("yo estoy al final");
+
+
+let ropa = ["Bufanda","Camisa","Guantes","jeans"]
+
+//ropa.length por longitud es 4 por lo elementos
+for(let i = 0; i < ropa.length; i++){
+    console.log(ropa[i])
+}
