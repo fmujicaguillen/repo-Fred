0130f86d7468beb133f98b812c895d985db16107
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-24 20:39:32 -0500

    update

--- Semana4/Semana5/Dia2/05-breakycontinue.js
+++ Semana4/Semana5/Dia2/05-breakycontinue.js
@@ -1,13 +1,25 @@
-let alumnos = ["Leonardo","Arturo","Gustavo","Ariana","Alan","Masami"];
+// let alumnos = ["Leonardo","Arturo","Gustavo","Ariana","Alan","Masami"];
 
-//algoritmo para que el usuario ingrese un nombre y busque si existe el alumbo y me muestre la posicion de ese alumno
-let eleccion = prompt("Ingrese el nombre del alumno");
+// //algoritmo para que el usuario ingrese un nombre y busque si existe el alumbo y me muestre la posicion de ese alumno
+// let eleccion = prompt("Ingrese el nombre del alumno");
 
-for (let i = 0; i < alumnos.length; i++){
-    if (eleccion == alumnos[i]){
-        console.log(`${eleccion} se encuentra en ${i}`);
-        break;//corta la ejecucion
-       }
-    console.log(`Buscando... en ${i}`);
+// for (let i = 0; i < alumnos.length; i++){
+//     if (eleccion == alumnos[i]){
+//         console.log(`${eleccion} se encuentra en ${i}`);
+//         break;//corta la ejecucion
+//        }
+//     console.log(`Buscando... en ${i}`);
    
+// }
+
+let notas = [17,15,10, 5, 14,20,7,9, 18,1,0,13]
+
+//saber las notas aprobatorias
+
+for (let i = 0; i < notas.length; i++){
+    if(notas[i] < 12){
+        continue;
+        
+    }
+    console.log(notas[i]);
 }
