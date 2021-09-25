d38fe1149ec5cc8e3cc303fc356839856b9ae1d7
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-25 17:38:08 -0500

    update

--- Semana4/Semana5/Dia3/03-ejercicio2.js
+++ Semana4/Semana5/Dia3/03-ejercicio2.js
@@ -38,5 +38,33 @@ let arrJuegos = [
  * 3. Segun la comparacion tener una lista de juegos a ofrecer
  * 4. agregar al carrito
  * 5. cobrar, indicar el total
- * 
- */
+ *  */
+
+let presupuesto = +prompt("¿Cuanto es su presupuesto?");
+
+function revisarPresupuesto(){
+    let juegosComprables = [] //una lista de los juegos que el usuario puede comprar
+    for(let i=0 ; i< arrJuegos.length; i++){
+        if(presupuesto >= arrJuegos[i].precio){
+            juegosComprables.push(arrJuegos[i])
+        }
+    }
+    return juegosComprables;
+}
+// agregaACarrito tiene q trabajar con la Lista de los juegos que el usuario puede comprar
+function agregarAcarrito(listaJuegos){
+    //confirm pregunta y obtiene un booleano
+   for (let j = 0 ;  j < listaJuegos.length ; j++){
+       let compra = confirm(`Deseas comprar ${listaJuegos[j]}`);
+       console.log(compra);
+
+   }
+
+}
+
+let listaComprables = revisarPresupuesto();
+console.table(listaComprables); // table en una regla
+// Despues de saber que juegos puede comprar el usuario 
+// le pregunto cual o cuales compra
+55
+agregarAcarrito(listaComprables);
