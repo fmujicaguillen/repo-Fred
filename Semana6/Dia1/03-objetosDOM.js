5400e217ac3063add1f1c4759b5ec0e989741b01
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-10-01 18:48:34 -0500

    update

--- Semana6/Dia1/03-objetosDOM.js
+++ Semana6/Dia1/03-objetosDOM.js
@@ -32,3 +32,31 @@ miCaja.innerHTML = "<h2>Titulo Caja</h2>";
 
 miCaja.style.backgroundColor = "purple";
 miCaja.style.color = "white";
+
+
+let misItems = document.getElementsByClassName("lista_item")
+console.log(misItems);
+
+//Array.from(htmlCollection), crea un arreglo
+let arregloItems = Array.from(misItems);
+
+console.log(arregloItems);
+
+//anotacion importante forEach, map, filter, find
+//forEach(function(item,posicion,array))
+
+arregloItems.forEach(function(item, indice,conjunto){
+    item.style.backgroundColor = "skyblue";
+    item.innerHTML = `Patata ${indice}`;
+    console.log("mostrando el arreglo", conjunto);
+});
+
+
+// obtener element de diferentes maneras
+// los querySelector y querySelectorAll funcionar de la misma manera que los selectores en css
+let miTexto = document.querySelector("#texto");
+console.log(miTexto);
+
+//querySelectorAll me da un NodeList
+let boxes = document.querySelectorAll(".box");
+console.log(boxes);
