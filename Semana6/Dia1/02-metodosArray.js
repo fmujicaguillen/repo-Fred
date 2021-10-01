fc8e7c28c4a0fe8384bc8f67b46566ddf5cb0ef8
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-30 20:47:40 -0500

    update

--- Semana6/Dia1/02-metodosArray.js
+++ Semana6/Dia1/02-metodosArray.js
@@ -1,10 +1,10 @@
 let frutas = ["fresa","mango","piña","lucuma"];
 
-// for (let i = 0 ; i < frutas.length ; i++){
-//     console.log(frutas[i]);
-// }
+// // for (let i = 0 ; i < frutas.length ; i++){
+// //     console.log(frutas[i]);
+// // }
 
-// recuerden los arreglos tambien son objetos
+// // recuerden los arreglos tambien son objetos
 
 frutas.forEach(function(fru){
     console.log(fru);
@@ -14,3 +14,59 @@ frutas.forEach(function(fru){
 // ForEach por cada elemento que encuentre vas a ejecutar una funcion 
 //y cada ve que se ejecute esa funcion vas a recibir c/item en orden y este item
 //lo podemos utilizar dentro de la funcion
+
+
+let energia = [89, 90, 86, 85, 100];
+// //la energia se va dividir en 2
+// // como podria tener un nuevo arreglo con los valores que representen la mitad de  la energia por cada item
+
+// let energiaCima = []
+
+// for(let i = 0; i < energia.length ; i++){
+//     energiaCima.push(energia[i]/2)
+// }
+// console.log(energiaCima)
+
+let energiaCima = energia.map(function(energy){
+    // let energiaTransformada = energy / 2;
+    // return energiaTransformada;
+    return energy / 2;
+});
+console.log(energiaCima);
+
+//FILTER
+
+let conductores = [
+    {
+        nombre:"Jose",
+        vacunado : true,
+    },
+    {
+        nombre:"Glenda",
+        vacunado: true,
+    },
+    {
+        nomre:"Osmar",
+        vacunado: false,
+    },
+]
+
+let puedePasar = conductores.filter(function(driver){
+    return driver.vacunado == true;
+
+});
+
+console.log(puedePasar)
+
+
+
+let dniDB = ["32312323","12345678","54353444"];
+
+let encontrado = dniDB.find(function(doc){
+    // return doc == "12345678";
+    if (doc.includes("44")){
+        return doc;
+    }
+})
+
+console.log(encontrado);
