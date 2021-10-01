05d78d4687a49c8b4a7d095437972bdd1b795614
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-09-30 19:41:40 -0500

    update

---
+++ Semana6/Dia1/02-metodosArray.js
@@ -0,0 +1,16 @@
+let frutas = ["fresa","mango","piña","lucuma"];
+
+// for (let i = 0 ; i < frutas.length ; i++){
+//     console.log(frutas[i]);
+// }
+
+// recuerden los arreglos tambien son objetos
+
+frutas.forEach(function(fru){
+    console.log(fru);
+
+})
+
+// ForEach por cada elemento que encuentre vas a ejecutar una funcion 
+//y cada ve que se ejecute esa funcion vas a recibir c/item en orden y este item
+//lo podemos utilizar dentro de la funcion
