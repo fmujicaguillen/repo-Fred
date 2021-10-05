85452640a646d4686fa7e5927ab8ac03901df3cf
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-10-05 12:41:40 -0500

    update

---
+++ Semana6/Dia2/02-eventos.js
@@ -0,0 +1,60 @@
+let divContenido = document.getElementById("contenido");
+let miBoton = document.createElement("button");
+
+miBoton.innerText = "Clickeame!";
+divContenido.appendChild(miBoton);
+
+//elementoHTML, addEventListener("evento,function(){}")
+miBoton.addEventListener("click",function(){
+    alert("Haz hecho click");
+})
+
+let peliculas = [
+    "Parasite",
+    "Shang Chi",
+    "Interstellar",
+    "Matrix",
+    "El bueno, el malo y el feo",
+    "Geo tormanta",
+    "Jhon Wick",
+    "Constantine",
+    "Godfather",
+    "El señor de los anillos",
+];
+
+peliculas.forEach(function(peli){
+    let nuevoParrafo = document.createElement("p");
+    nuevoParrafo.innerHTML =peli;
+    divContenido.appendChild(nuevoParrafo);
+
+
+    nuevoParrafo.style.cursor = "pointer";
+    nuevoParrafo.addEventListener("dblclick", function(){
+        alert(`Hiciste click en ${peli}`);
+        nuevoParrafo.style.backgroundColor = "steelblue";
+        nuevoParrafo.style.color = "red";
+    });
+});
+
+//
+
+let miInput = document.createElement("input");
+miInput.setAttribute("type","password");
+divContenido.appendChild(miInput);
+
+
+let btnVer = document.createElement("button");
+btnVer.innerText = "ver";
+divContenido.appendChild(btnVer);
+
+let esVisible = false; //flag
+btnVer.addEventListener("click",function(){
+    esVisible = !esVisible;
+
+    if(esVisible == false){
+        miInput.setAttribute("type","text");
+
+    }else{
+        miInput.setAttribute("type","password")
+    }
+});
