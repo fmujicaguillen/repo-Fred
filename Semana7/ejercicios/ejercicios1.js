bac9dc59be8a1a7468ef3ff5b04e3908a3909bba
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-10-05 19:39:14 -0500

    update

---
+++ Semana7/ejercicios/ejercicios1.js
@@ -0,0 +1,33 @@
+//Para esta parte usar if y for
+
+// Ej1, crear una funcion que reciba un arreglo y un nombre
+// si el nombre existe dentro de la funciona retornar la posicion
+// del nombre encontrado, en caso de no encontralo retornar -1
+
+// EJERCICIO RUC - MODULO 11
+
+//
+
+function verificarRUC(ruc){
+    // verifiquemos que el RUC tenga 11 digitos
+
+    if(ruc.length == 11){
+        // es correcto porque eventualmente en este bloque de codigo retornare un true
+        let factorChequeo = [5,4,3,2,7,6,5,4,3,2]
+        let total = 0;
+        for(let i=0; i < factorChequeo.length ; i++){
+            let prod = ruc[i] * factorChequeo[i]
+            total += prod
+
+         }
+         let modulo = total % 11;
+         let verificador = 11 - modulo;
+         if(verificador==ruc[10]){
+             return true
+         }
+    }
+    return false
+    
+}
+
+console.log(verificarRUC("10443199212"));
