e4d63fea1caed6e1fe7f9f07ac28c4eb626e3b6a
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-10-14 21:23:38 -0500

    update

---
+++ Semana8/Dia1/04-asincronia.js
@@ -0,0 +1,9 @@
+console.log('1. Estamos jugando monopolio y nos dio hambre');
+
+setTimeout(function(){
+    console.log("2. Esperar al Rappi hasta que llegue")
+},2000)
+
+console.log('3. llega el Rappi y lo comemos')
+
+//JAVASCRIPT SOLAMENTE PUEDE EJECUTAR 01 TAREA A LA VEZ
