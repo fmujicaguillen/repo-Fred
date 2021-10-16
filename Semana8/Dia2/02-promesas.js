47980c9350c11ee8cb5044b3cf050fce7b533a1b
fmujicaguillen <89620987+fmujicaguillen@users.noreply.github.com>
2021-10-15 19:10:11 -0500

    update

---
+++ Semana8/Dia2/02-promesas.js
@@ -0,0 +1,27 @@
+const hornearTorta = () =>{
+    return new Promise((resolve,reject) =>{
+        setTimeout(() => {
+
+            //Aqui estoy forzando a que sea true o false
+            // comunmente terndriamos una operacion, buscar un registro
+            //obtener un elemento, obtener datos
+            let estado = false;
+            if(estado == true){
+                resolve("La torta esta Ready!");
+            }
+            else{
+                reject("Pos se quemó :(");
+            }
+        },5000);
+
+    });
+};
+
+
+hornearTorta()
+    .then((tortaHorneada)=>{
+    console.log(tortaHorneada);
+    }).catch((tortaQuemada)=>{
+
+    console.log(tortaQuemada);
+    })
